a59c02658ffe68bf81e208b53a475742b819ea32
Dave Laing <dave.laing.80@gmail.com>
2017-07-25 10:26:55 +1000

    Adds the first part of the Hydra support.
    Once we switch to `qfpl` as the default working branch and add
     a `hydra` user in the organization to push the compiling changes
    from the `qfpl` branch to the `master` branch we'll be good to go.
    
    We may want to turn GHC 8.2.1 off for a while or wait for the
    dust to settle before we use Hydra as a gatekeeper.

---
+++ ci/ci.nix
@@ -0,0 +1,56 @@
+{ supportedSystems ? ["x86_64-linux"]
+, supportedCompilers ? [ "ghc7103" "ghc802" "ghc821" ] 
+, papaPackages ? [
+    "papa-example"
+    "papa"
+    "papa-base"
+    "papa-base-export"
+    "papa-base-implement"
+    "papa-bifunctors"
+    "papa-bifunctors-export"
+    "papa-bifunctors-implement"
+    "papa-lens"
+    "papa-lens-implement"
+    "papa-lens-export"
+    "papa-semigroupoids"
+    "papa-semigroupoids-implement"
+    "papa-semigroupoids-export"
+    "papa-x"
+    "papa-x-implement"
+    "papa-x-export"
+    ]
+}:
+
+with (import <nixpkgs/pkgs/top-level/release-lib.nix> { inherit supportedSystems; });
+
+let
+  pkgs = import <nixpkgs> {};
+
+  configurations = 
+    pkgs.lib.listToAttrs (
+      pkgs.lib.concatMap (papaPackage: 
+        pkgs.lib.concatMap (compiler: 
+          pkgs.lib.concatMap (system: 
+            [{name = "haskell.packages." + compiler + "." + papaPackage + "." + system ; value = {inherit papaPackage compiler system;};}]
+          ) supportedSystems
+        ) supportedCompilers
+      ) papaPackages
+    );
+
+  jobs =
+      pkgs.lib.mapAttrs (name: configuration: 
+          let
+            papaPackage = configuration.papaPackage;
+            compiler = configuration.compiler; 
+            system = configuration.system; 
+            pkgs = pkgsFor system;
+            jailbreakLatest = p: if compiler == "ghc821" then pkgs.haskell.lib.doJailbreak p else p;
+            haskellPackages = pkgs.haskell.packages.${compiler}.override { 
+              overrides = self: super: (import ../. self);
+            };
+            pkg = haskellPackages.callPackage (../. + "/${papaPackage}/") {};
+          in
+            pkg
+      ) configurations;
+in
+  jobs

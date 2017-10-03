e268ed8eab07ed726fc644b1a89f66a4b38d8411
Dave Laing <dave.laing.80@gmail.com>
2017-10-04 09:59:13 +1000

    Bumps nixpkgs to 17.09

--- ci/jobsets.nix
+++ ci/jobsets.nix
@@ -18,7 +18,7 @@
             "keepnr": 5,
             "inputs": {
                 "papa": { "type": "git", "value": "https://github.com/qfpl/papa.git master", "emailresponsible": false },
-                "nixpkgs": { "type": "git", "value": "https://github.com/NixOS/nixpkgs.git release-17.03", "emailresponsible": false }
+                "nixpkgs": { "type": "git", "value": "https://github.com/NixOS/nixpkgs.git release-17.09", "emailresponsible": false }
             }
         }
     }
